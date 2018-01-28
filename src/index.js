3cfedde6bc259a356429003fffd449af2fa58378
Brian Yamasaki <brian.yamasaki@gmail.com>
2018-01-27 22:15:26 -0800

    add registerServiceWorker to the project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
 import App from './containers/app';
+import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'sanitize.css/sanitize.css';
 import './index.css';
 
-const target = document.querySelector('#root');
-
-render(
+const body = (
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <div>
-        <App />
-      </div>
-    </ConnectedRouter>
-  </Provider>,
-  target
+      <ConnectedRouter history={history}>
+          <div className="container">
+              <App />
+          </div>
+      </ConnectedRouter>
+  </Provider>
 );
+
+ReactDOM.render(body, document.getElementById('root'));
+registerServiceWorker();
